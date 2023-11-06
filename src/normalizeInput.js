89da2378979073d14b94f804248cda145759ce6e
seangloveland <sean@brandboom.com>
2023-11-06 14:43:48 -0800

    Normalize input no longer uses positive lookbehind

--- src/normalizeInput.js
+++ src/normalizeInput.js
@@ -7,7 +7,7 @@ const emojiRegex = createEmojiRegex();
 export default (input: string): string => {
   return input
     .replace(emojiRegex, ' ')
-    .replace(/(?<=\s|^)([a-z0-9.-_@])\s?(?=[a-z0-9.-_@](?:\s|$))/g, '$1')
+    .replace(/(?:\s|^)([a-z0-9.-_@])\s?([a-z0-9.-_@])(?:\s|$)/g, '$1')
     .replace(/\s+at\s+/g, '@')
     .replace(/\s+dot\s+/g, '.')
     .replace(/\s*<at>\s*/g, '@')
